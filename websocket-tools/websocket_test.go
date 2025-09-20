fabcf5f616078cdf4210cee96c0c5ef97ef39d32
ZJQ <wy17674094293@163.com>
2025-09-20 16:38:26 +0800

    feat:websocket完成

---
+++ websocket-tools/websocket_test.go
@@ -0,0 +1,21 @@
+package websockettools
+
+import (
+	"context"
+	"testing"
+	"time"
+	"github.com/gin-gonic/gin"
+)
+
+func TestStream(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	stream := NewWebSocketStream(ctx, 3*time.Second, 6*time.Second, []string{`^localhost$`, `^127\.0\.0\.1$`})
+
+	route := gin.Default()
+	route.GET("/ws", stream.GinHandler)
+
+	if err := route.Run(":8080"); err != nil {
+		t.Error(err)
+	}
+}
