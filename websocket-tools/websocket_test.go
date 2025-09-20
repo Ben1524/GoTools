ca475bc99c29be9a88e68a7b611e40293aa69739
ZJQ <wy17674094293@163.com>
2025-09-20 21:56:59 +0800

    feat:增加关闭测试用例

--- websocket-tools/websocket_test.go
+++ websocket-tools/websocket_test.go
@@ -19,3 +19,22 @@ func TestStream(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestClose(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	stream := NewWebSocketStream(ctx, 3*time.Second, 6*time.Second, []string{`^localhost$`, `^127\.0\.0\.1$`})
+
+	route := gin.Default()
+	route.GET("/ws", stream.GinHandler)
+
+	// 5秒后关闭服务
+	go func() {
+		time.Sleep(15 * time.Second)
+		stream.Close()
+	}()
+
+	if err := route.Run(":8080"); err != nil {
+		t.Error(err)
+	}
+}
